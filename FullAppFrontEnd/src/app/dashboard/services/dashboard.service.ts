13207b5c934dda95d929f73d7d5c3af6061458a3
zcmjs <zcm@interia.pl>
2019-12-02 00:25:08 +0100

    ustawienie serwera proxy

--- FullAppFrontEnd/src/app/dashboard/services/dashboard.service.ts
+++ FullAppFrontEnd/src/app/dashboard/services/dashboard.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {DashBoardDto} from '../dtos/DashboardDto';
 
@@ -12,11 +12,22 @@ export class DashboardService {
   }
 
   getCustomizedData(): Observable<DashBoardDto> {
-    return this.http.get<DashBoardDto>('http://localhost:8080/dashboard/welcome');
+    return this.http.get<DashBoardDto>('/api/dashboard/welcome');
   }
 
   getCustomizedDataWithParameter(name): Observable<DashBoardDto> {
-    return this.http.get<DashBoardDto>(`http://localhost:8080/dashboard/welcome/${name}`);
+    return this.http.get<DashBoardDto>(`/api/dashboard/welcome/${name}`, {headers: this.getHeader()});
+  }
+
+  private getHeader() {
+    const username = "macon";
+    const password = "ssac";
+    const encodedUserData = btoa(`${username}:${password}`);
+    const bassicc = `Basic ${encodedUserData}`;
+
+    return new HttpHeaders({
+      'Authorization': bassicc
+    });
   }
 
 }
